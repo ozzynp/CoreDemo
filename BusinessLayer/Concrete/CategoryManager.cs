8bc0c8edb4aba3f3bb61fdfe0263975cb90a3a7c
ozzynp <oz.zeynep.peace@gmail.com>
2024-10-02 13:25:01 +0300

    Initial commit

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,53 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository cr;
+
+        public CategoryManager()
+        {
+            cr = new EfCategoryRepository();
+        }
+        public void AddCategory(Category category)
+        {
+            repo.Insert(category);
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            if (category.CategoryName != "" && category.CategoryDescription != null && category.CategoryName.Length >= 5)
+            {
+                repo.Delete(category);
+            }
+            else
+            {
+                //Hata MEsajı
+            }
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //erişim belirleyici türü - Değişken türü - isim { get ; set }
+        [Key]
+        public int CategoryID { get; set; }
+
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [StringLength(150)]
+        public string CategoryDescription { get; set; }
+        public bool CategoryStatus { get; set; }
+
+        public List<Blog> Blogs { get; set; }
+    }
+}
